76cecdbd514f99ad7a23401948106bdfe6bec071
pierre aubert <pierre@spinorama.org>
2025-02-13 20:23:24 +0100

    fix: pyright happy

--- gll2txt.py
+++ gll2txt.py
@@ -6,6 +6,8 @@ import time
 import zipfile
 
 import pywinauto as win
+from pywinauto.application import Application, WindowSpecification
+from pywinauto.controls.win32_controls import ComboBoxWrapper, ListBoxWrapper
 
 from logger import log_message
 
@@ -51,31 +53,39 @@ def dump(widget) -> None:
         keys = widget.keys()
         for key in keys:
             dump(widget[key])
-    try:
-        items = widget.item_texts()
-        log_message(
-            logging.DEBUG,
-            "- item texts ---------------------------------------------------------",
-        )
-        log_message(logging.DEBUG, items)
-    except Exception:
-        pass
-    try:
-        items = widget.print_control_identifier()
-        log_message(
-            logging.DEBUG,
-            "- control identifiers ------------------------------------------------",
-        )
-        log_message(logging.DEBUG, items)
-    except Exception:
-        pass
+
+    if isinstance(widget, ComboBoxWrapper) or isinstance(widget, ListBoxWrapper):
+        try:
+            items = widget.item_texts()
+            log_message(
+                logging.DEBUG,
+                "- item texts ---------------------------------------------------------",
+            )
+            log_message(logging.DEBUG, items)
+        except Exception:
+            pass
+
+    if isinstance(widget, WindowSpecification):
+        try:
+            items = widget.print_control_identifier()
+            log_message(
+                logging.DEBUG,
+                "- control identifiers ------------------------------------------------",
+            )
+            log_message(logging.DEBUG, items)
+        except Exception:
+            pass
+
+
+def process_type_keys(input: str) -> str:
+    return input.replace("+", "{+}")
 
 
 def load_gll(app, gll_file: str):
     openGLLFile = app.OpenGLLFile
     openGLLFile.wait("visible")
     openGLLFile.set_focus()
-    openGLLFile.type_keys(gll_file, with_spaces=True)
+    openGLLFile.type_keys(process_type_keys(gll_file), with_spaces=True)
     openGLLFile.type_keys("{ENTER}")
 
 
@@ -88,7 +98,7 @@ def load_config(app, view, config_file: str | None):
     openConfigFile.wait("visible")
     openConfigFile.set_focus()
     openConfigFile.type_keys(
-        config_file.replace("+", "{+}").replace("/", "\\"), with_spaces=True
+        process_type_keys(config_file).replace("/", "\\"), with_spaces=True
     )
     openConfigFile.type_keys("{ENTER}")
 
@@ -171,7 +181,7 @@ def build_spl_filename(
 ) -> str:
     speaker_dir = build_speaker_dir(output_dir, speaker_name, config_file)
     return "{0}\\{1} -M{2}-P{3}.txt".format(
-        speaker_dir, os.basename(speaker_dir), meridian[:-1], parallel[:-1]
+        speaker_dir, os.path.basename(speaker_dir), meridian[:-1], parallel[:-1]
     ).replace("/", "\\")
 
 
@@ -232,7 +242,9 @@ def extract_spl(
 
     for m in meridians:
         for p in parallels:
-            output_file = build_spl_filename(output_dir, speaker_name, m, p)
+            output_file = build_spl_filename(
+                output_dir, speaker_name, config_file, m, p
+            )
             if os.path.exists(output_file):
                 log_message(
                     logging.INFO,
@@ -250,7 +262,7 @@ def extract_spl(
             export.wait("visible")
             # copy filename
             export.type_keys("%n{BACKSPACE}")
-            export.type_keys(output_file, with_spaces=True)
+            export.type_keys(process_type_keys(output_file), with_spaces=True)
             export.type_keys("{ENTER}")
             export.wait_not("visible")
             log_message(
@@ -276,7 +288,7 @@ def extract_sensitivity(app, view, output_dir, speaker_name, config_file):
     # copy filename
     export.type_keys("%n{BACKSPACE}")
     sensitivity_file = build_sensitivity_filename(output_dir, speaker_name, config_file)
-    export.type_keys(sensitivity_file, with_spaces=True)
+    export.type_keys(process_type_keys(sensitivity_file), with_spaces=True)
     export.type_keys("{ENTER}")
     export.wait_not("visible")
     app.wait_cpu_usage_lower(threshold=5)
@@ -297,7 +309,7 @@ def extract_maxspl(app, view, output_dir, speaker_name, config_file):
     # copy filename
     export.type_keys("%n{BACKSPACE}")
     maxspl_file = build_maxspl_filename(output_dir, speaker_name, config_file)
-    export.type_keys(maxspl_file, with_spaces=True)
+    export.type_keys(process_type_keys(maxspl_file), with_spaces=True)
     export.type_keys("{ENTER}")
     export.wait_not("visible")
     app.wait_cpu_usage_lower(threshold=5)
@@ -356,14 +368,16 @@ def extract_speaker(
 
     # Rest of the function remains the same, but use log_message instead of print
     if not check_all_files(output_dir, speaker_name, config_file):
-        app = win.Application(backend="win32").start(ease_full)
+        app = Application(backend="win32").start(ease_full)
         load_gll(app, gll_file)
         # not ideal
         gll_file = os.path.basename(gll_file)
         view = app["ViewGLL: {} [{}]".format(speaker_name, gll_file)]
         load_config(app, view, config_file)
+        time.sleep(1)
         view.type_keys("{F5}")
         set_parameters(app)
+        time.sleep(1)
         extract_spl(app, view, output_dir, speaker_name, config_file)
         extract_sensitivity(app, view, output_dir, speaker_name, config_file)
         extract_maxspl(app, view, output_dir, speaker_name, config_file)
